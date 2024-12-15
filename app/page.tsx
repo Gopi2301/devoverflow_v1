bfbfc429d685eebd2730e0309ee5cb62d89d2641
GopiNath <gopinaths2301@gmail.com>
2024-12-15 15:54:43 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+const Home = () => (
+  <h1 className="text-3xl text-violet-700 font-black ">
+    Welcome to the world of next.js
+  </h1>
+);
+export default Home;
